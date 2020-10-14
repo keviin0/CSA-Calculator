7bf9387279d680a9b736447fc6b3702a9e11ccee
Kevvinn <kevinhu229@gmail.com>
2020-10-14 08:46:53 -0700

    add base JFrame

---
+++ Main.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import javax.swing.JFrame;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        JFrame frame = new JFrame("CSA Calclator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(600, 800);
+        frame.setResizable(false);
+        
+    }
+}
