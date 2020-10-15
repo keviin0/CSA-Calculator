0061194aac40a14476b03853fc9994064ec325dc
Kevvinn <kevinhu229@gmail.com>
2020-10-14 19:43:04 -0700

    Add button panel with 1-9 buttonds

---
+++ Calculator.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Calculator{
+
+    private JFrame frame;
+    private int butRow = 6;
+    private int butColumn = 4;
+
+    public Calculator()
+    {
+        gui();
+    }
+
+    public void gui()
+    {
+        frame = new JFrame("CSA Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(600, 800);
+        frame.setResizable(true);
+        frame.setMinimumSize(new Dimension(320, 475));
+        
+        frame.add(new NumPanel());
+    }
+
+    class NumPanel extends JPanel 
+    {
+        private int numbers = 0;
+
+        public NumPanel() {
+            //*Numbers 1 - 9
+            GridLayout butGrid = new GridLayout(butRow, butColumn, 2, 2);
+            setLayout(butGrid);
+            for(int row = 2; row < butRow - 1; row++) {
+                for(int column = 0; column < butColumn - 1; column++){
+                    numbers++;
+                    JButton tempBut = new JButton(Integer.toString(numbers));
+                    add(tempBut).setLocation(row, column);
+                    tempBut.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println("I was clicked!");
+                        }         
+                    });                    
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args){
+        new Calculator();
+    }
+
+}
