893dcaced1a80eed4126b88ab2d590ce4e868ce6
AdityaS1426 <adisurapaneni@gmail.com>
2020-10-30 08:30:23 -0700

    Added Minesweeper game.

---
+++ src/ui/MineSweeperGame.java
@@ -0,0 +1,31 @@
+package ui;
+
+import javax.swing.JFrame;
+import java.awt.Component;
+
+public class MineSweeperGame extends JFrame
+{
+    private static final int WIDTH = 810;
+    private static final int HEIGHT = 510;
+    private MGame game;
+
+    public MineSweeperGame()
+    {
+        super("Mine Sweeper");
+        setSize(WIDTH,HEIGHT);
+
+        game = new MGame();
+
+        ((Component)game).setFocusable(true);
+        getContentPane().add(game);
+
+        setVisible(true);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public static void main( String args[] )
+    {
+        MineSweeperGame run = new MineSweeperGame();
+    }
+}
