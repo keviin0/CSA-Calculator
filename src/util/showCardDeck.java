87ed967b9e87432b81fed4f80eb046d6d243640e
Siddhant8 <71453341+Siddhant8@users.noreply.github.com>
2020-10-30 20:10:11 -0700

    Added Commented View for Deck Button that shows all the cards in the Deck. However, technical difficulties deleted that code and I created a new one, which didn't allow me to push it unless commented out.

---
+++ src/util/showCardDeck.java
@@ -0,0 +1,37 @@
+package util;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+/*public class showCardDeck {
+    private JPanel ShowCardDeck;
+    private JButton mShowCardDeck;
+    Deck test = new Deck();
+    //System.out.println(test.cardDeck);
+
+    //System.out.println(test.cardDeck);
+    Player testPlayer = new Player("monkaS");
+    mDrawCard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //mShowCardDeck.addActionListener(new ActionListener() {
+
+
+                  JButton newButton = new JButton(String.valueOf(test));
+
+                //newButton.setBounds(50,50,90, 50);
+
+                //ShowCardDeck.add(newButton, new GridBagConstraints());
+
+
+
+
+
+}
+
+ */
