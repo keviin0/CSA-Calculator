87c7e6abc630c34c2cbe270354c95875cc67adcb
Siddhant8 <71453341+Siddhant8@users.noreply.github.com>
2020-11-11 18:27:23 -0800

    Added to GoFish.

--- src/ui/GoFish.java
+++ src/ui/GoFish.java
@@ -1,48 +1,95 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GoFish extends JFrame {
-    public void setVisible(boolean b) {
+public class GoFish extends JFrame{
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                GoFish frame = new GoFish();
+                frame.setSize(300,300);
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        ArrayList<Integer> newDeck = new ArrayList<Integer>();
+        newDeck.add(0);
+        newDeck.add(1);
+        newDeck.add(2);
+        newDeck.add(3);
+        newDeck.add(4);
+        newDeck.add(5);
+        newDeck.add(6);
+        newDeck.add(7);
+        newDeck.add(8);
+        newDeck.add(9);
+        newDeck.add(10);
+        newDeck.add(11);
+        newDeck.add(12);
+        newDeck.add(14);
+        newDeck.add(15);
+        Collections.shuffle(newDeck);
+        System.out.println(newDeck);
+        List<Integer> gameDeck = newDeck.subList(0, 5);
+        List<Integer> pickDeck = newDeck.subList(5, 15);
+        System.out.println(pickDeck);
+        System.out.println(gameDeck);
+        Scanner scanning = new Scanner(System.in);
+        System.out.println("Pick a card");
+        int cardPick = scanning.nextInt();
+        System.out.println(cardPick);
+        boolean isTrue = pickDeck.contains(cardPick);
+        if (isTrue) {
+            System.out.println("correct");
+        } else{
+            System.out.println("wrong");
+            System.out.println("Draw A Card");
+            Integer g = pickDeck.get(0);
+            System.out.println(g);
+            gameDeck.add(g);
+            System.out.println(gameDeck);
+        }
+
     }
+
     public GoFish(){
-        //View Code
 
-        JFrame frame = new JFrame("Card Games");
-
-        frame.setSize(340, 320);
-
-        JButton cardDeck = new JButton("Deck");
-        //cardDeck.setSize(new Dimension(10, 10));
-        //cardDeck.setLocation(10, 30);
-        //frame.add(BorderLayout.EAST, cardDeck);
-        //Creating the Menu and adding parts to it
-        JMenuBar menu = new JMenuBar();
-        JMenu gameMenu = new JMenu("GoFish");
-
-        menu.add(gameMenu);
-
-        //Creating the panel at bottom and adding components
-        JPanel ask = new JPanel();
-
-        JLabel question = new JLabel("Pick a Card");
-        JTextField field = new JTextField(12);
-        JButton enter = new JButton("Enter");
-
-        ask.add(question);
-        ask.add(field);
-        ask.add(enter);
-
+        super("GoFish");
+        setBounds(100, 100, 418, 315);
+        Container c = getContentPane();
         JPanel panel = new JPanel();
-        JButton name = new JButton("Enter Name");
-        panel.add(name);
+
+        JTextField f = new JTextField("Pick a Card", 10);
+        JButton b = new JButton("Enter");
+
+        panel.add(f);
+        panel.add(b);
+        c.add(panel);
+
+
+        List<String> allMatches = new ArrayList<String>();
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Using BorderLayout
-        //frame.add(BorderLayout.SOUTH,ask);
-
-        //frame.add(BorderLayout.NORTH, menu);
+        JButton button = new JButton("Draw Card");
     }
+
+
+
+
+
+
 }
--- src/util/GoFishCard.java
+++ src/util/GoFishCard.java
@@ -1,35 +1,21 @@
 package util;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
 
 public class GoFishCard {
-    public GoFishCard() {
 
-        ArrayList<String> cardDeck = new ArrayList<String>();
 
-        String[] suites = {"Diamond", "Spade", "Club", "Heart"};
-        String[] values = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-                "Nine", "Ten", "Jack", "Queen", "King"};
+    public enum Suite {
+        Spade, Club, Diamond, Heart;
+        private static Suite[] suites = Suite.values();
 
+        public static Suite getSuite(int i) {
+            return Suite.suites[i];
+        }
     }
 
-    private String[] card(String[] suites, String[] values) {
 
 
-        for (int i = 0; i <= 4; i++) ;
-        for (int j = 0; j <= 13; j++) ;
-        String[] card = card(suites, values);
-
-        return card;
-    }
-
-    private int shuffle() {
-        int n = 52;
-        return shuffle();
-    }
-    private boolean player(){
-        boolean player = true;
-        return true;
-    }
 }
