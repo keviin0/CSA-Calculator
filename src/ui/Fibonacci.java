a71ebddcff0973a467d6b53299031ca8798afba6
Sean Tran <sgtran@gmail.com>
2020-11-13 22:31:01 -0800

    Added MagicFibonacci card game

---
+++ src/ui/Fibonacci.java
@@ -0,0 +1,15 @@
+package ui;
+
+public class Fibonacci {
+
+    public void Fibonacci(){
+
+        // This will be the array used to store the magic cards
+        int[][] fibArray = new int[10][20];
+
+        /* Here should be the code
+        that attempts to use recursion to display magic cards */
+
+
+    }
+}
